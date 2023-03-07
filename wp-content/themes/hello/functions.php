a64da3b565774cf8fbacc59d3a7627511847bff7
AlexAce13 <oleksandr_klishch@ex.ua>
2023-03-07 16:51:52 +0200

    second commit

---
+++ wp-content/themes/hello/functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Hello
+ * @since Hello 1.0
+ */
+
+// Enhance the theme by hooking into WordPress.
+require get_template_directory() . '/inc/template-functions.php';
